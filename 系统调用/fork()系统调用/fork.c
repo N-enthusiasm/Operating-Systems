90812784332ec341cf8f147b5302de5597d39069
Enthusiasm <61376966+N-enthusiasm@users.noreply.github.com>
2020-11-04 15:23:31 +0800

    Add files via upload

---
+++ 系统调用/fork()系统调用/fork.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main(int argc,char *argv[])
+{
+	printf("hello world (pid:%d)\n",(int)getpid());
+	int rc=fork();
+	if(rc<0)
+	{
+		fprintf(stderr,"fork failed\n");
+		exit(1);
+	}
+	else if(rc==0)
+	{
+		printf("I am child (pid:%d)\n",(int)getpid());
+	}
+	else
+	{
+		printf("I am parent of %d,my pid is %d\n",rc,(int)getpid());
+	}
+	return 0;
+}
